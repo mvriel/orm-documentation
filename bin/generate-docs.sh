df16bf8c13a93602eacc118035ffb2a53ee2c721
Mike van Riel <mike.vanriel@naenius.com>
2011-09-27 00:22:16 +0200

    Added DocBlox support to documentation generation

--- bin/generate-docs.sh
+++ bin/generate-docs.sh
@@ -8,3 +8,8 @@ sphinx-build en build
 
 sphinx-build -b latex en build/pdf
 rubber --into build/pdf --pdf build/pdf/Doctrine2ORM.tex
+
+wget https://github.com/doctrine/common/zipball/master -O /tmp/doctrine-common-source.zip
+unzip /tmp/doctrine-common-source.zip -d /tmp/doctrine-common-source
+docblox -d /tmp/doctrine-common-source/doctrine-common-* -t build/api --defaultpackagename=Doctrine --title=Doctrine --ignore="tests/*"
+rm -rf /tmp/doctrine-common-source*
--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -2,3 +2,8 @@
 sudo apt-get install python25 python25-dev texlive-full rubber
 sudo easy_install pygments
 sudo easy_install sphinx
+
+sudo apt-get install wget
+sudo pear channel-discover pear.michelf.com
+sudo pear channel-discover pear.docblox-project.org
+sudo pear install docblox/docblox-beta
